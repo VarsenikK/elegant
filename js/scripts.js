e8ac4e452c6d92422b052c76d7481fb241a8c714
VarsenikK <kambarianv@gmail.com>
2020-12-05 18:04:56 +0200

    add dialog

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,28 @@ $(document).ready(function(){
             },
 
             1000:{
-                items:3
+                items:1
             }
         }
     });
 });
+
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
+
+
